8f6de6beb344af15d253bdd18fcfb91951bde8dc
vikasgaur56 <50748463+vikasgaur56@users.noreply.github.com>
2024-09-16 17:27:36 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 14454;
+let accoutnEmail="vikas@gmail.com";
+var accPass="vikas";
+accountCity="Jaipur";
+let accountState;
+//accountId=2// not allowed
+console.log(accountId);
+console.table([accountId,accountCity,accPass,accountState])
+/*Prefer not to use var because of issue in block scope and function scope */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict" //treat all js code as newer version//
+// alert(3+3)// we are using node.js not brower
+
+//number=>2 
+// bigint
+//string=>""
+//boolean=>true/false
+//null=> standalone value
+//undefined=>
+//symbol=>unique
+
+
+// object
+
