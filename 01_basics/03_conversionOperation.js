19c8656aac2ca462a6db8c84de2b94e9ec3be736
vikasgaur56 <50748463+vikasgaur56@users.noreply.github.com>
2024-09-16 17:50:20 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,6 @@
+let score="33";
+//console.log(typeof score);
+//console.log(Number(score));
+let myName="";
+console.log(Boolean(myName)) //true if it has value
+console.log(Boolean(myName))//false if it does not have value
