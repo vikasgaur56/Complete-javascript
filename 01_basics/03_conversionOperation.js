39438eb42c064e7eeed3000eb19a9d5a71a88b16
vikasgaur56 <50748463+vikasgaur56@users.noreply.github.com>
2024-09-20 11:37:32 +0000

    typeconversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,6 +1,9 @@
 let score="33";
 //console.log(typeof score);
 //console.log(Number(score));
-let myName="";
-console.log(Boolean(myName)) //true if it has value
+//"33"= 33
+//"33nbj"=nan
+
+let myName="vikas";
+console.log(Boolean(myName)) //true if it has value 1 => true 0 => false
 console.log(Boolean(myName))//false if it does not have value
