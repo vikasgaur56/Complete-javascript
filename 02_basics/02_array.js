f3de4aa51c7b4f744796864a29c95c8c9370ed08
vikasgaur56 <50748463+vikasgaur56@users.noreply.github.com>
2024-10-02 03:54:52 +0000

    02_array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,8 @@
+const array = ["vikas",'yash','snoop']
+const array2=[1,2,3,5,6]
+const array3 =['vikas','yashika','ap']
+
+const result = array3.push(array)
+
+// const concArr = array2.concat(array3)
+console.log(result)
