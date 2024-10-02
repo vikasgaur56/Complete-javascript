9a81f452d938fc90572193f0638976b5e39437b2
vikasgaur56 <50748463+vikasgaur56@users.noreply.github.com>
2024-10-02 03:54:35 +0000

    01_array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,17 @@
+const array = ["vikas",'yash','snoop']
+const array2=[1,2,3,5,6]
+const array3 =['vikas','yashika','ap']
+
+const result = array.push(array2)
+// console.log(result)
+
+// array.push('akshay')
+// array.pop()
+// array.unshift('abhay')
+// array.shift()
+
+// console.log(array.join())  // change array to string format
+// console.log(array)
+
+// console.log(array2.slice(1,3)) //original array is not changed
+// console.log(array2.splice(1,3)) //original array is changed
